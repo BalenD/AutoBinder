508b87ac4b348a39a51a910558477879bb6493dd
BalenD <balen1996@hotmail.com>
2019-11-22 01:44:03 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function autoBind(obj) {
+  const keys = Reflect.ownKeys(obj.constructor.prototype);
+  keys.forEach((key) => {
+    if (typeof obj[key] === 'function') {
+      obj[key] = obj[key].bind(obj);
+    }
+  });
+};
