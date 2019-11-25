faad2c3e1a475da3b915d3d8a32e90fc1d79b143
BalenD <balen1996@hotmail.com>
2019-11-25 14:52:05 +0100

    made it work for inheritance as well

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-module.exports = function autoBind(obj) {
-  const keys = Reflect.ownKeys(obj.constructor.prototype);
-  keys.forEach((key) => {
-    if (typeof obj[key] === 'function') {
-      obj[key] = obj[key].bind(obj);
-    }
+module.exports = function autoBind(obj, exclude) {
+  const ObjAndKeys = [];
+  while (obj !== Object.prototype) {
+    ObjAndKeys.push([Reflect.getPrototypeOf(obj), Reflect.ownKeys(obj.constructor.prototype)]);
+    obj = Reflect.getPrototypeOf(obj);
+  }
+  ObjAndKeys.forEach((val) => {
+    val[1].forEach((prop) => {
+      // TODO: add  excludes
+      const desc = Reflect.getOwnPropertyDescriptor(val[1], prop);
+      if (desc && typeof desc.value === 'function') {
+        obj[prop] = obj[prop].bind(obj);
+      }
+    });
   });
 };
